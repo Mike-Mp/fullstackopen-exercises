5063bf6f21d06480214339120c04de25bb2a40e9
Mike-Mp <devmichalis@protonmail.com>
2020-05-12 09:42:07 +0300

    complete phonebook part2b

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { useState } from "react";
 
 const Header = ({ course }) => {
   return <h1>{course.name}</h1>;
@@ -9,9 +8,9 @@ const Header = ({ course }) => {
 const Content = ({ course }) => {
   return (
     <>
-      <Part part={course.parts[0].name} exercises={course.parts[0].exercises} />
-      <Part part={course.parts[1].name} exercises={course.parts[1].exercises} />
-      <Part part={course.parts[2].name} exercises={course.parts[2].exercises} />
+      {course.parts.map((part) => (
+        <Part key={part.name} part={part.name} exercises={part.exercises} />
+      ))}
     </>
   );
 };
@@ -24,82 +23,76 @@ const Part = ({ part, exercises }) => {
   );
 };
 
-const Total = ({ exercises }) => {
-  const exercisesSum = exercises.parts.map((obj1) => {
+const Total = ({ course }) => {
+  const exercisesSum = course.parts.map((obj1) => {
     return obj1.exercises;
   });
   return <p>Number of exercises {exercisesSum.reduce((a, b) => a + b, 0)}</p>;
 };
 
-const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
-  };
+const Course = ({ course }) => {
   return (
     <div>
       <Header course={course} />
       <Content course={course} />
-      <Total exercises={course} />
+      <Total course={course} />
     </div>
   );
 };
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return (
-      <div>
-        <p>the app is used by pressing the buttons</p>
-      </div>
-    );
-  }
+const App = () => {
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
   return (
-    <div>
-      <p>button press history: {props.allClicks.join(" ")} </p>
-    </div>
+    <>
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </>
   );
 };
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
-
-const App2 = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAllClicks] = useState([]);
-
-  const handleLeftClick = () => {
-    setAllClicks(allClicks.concat("L"));
-    setLeft(left + 1);
-  };
-
-  const handleRightClick = () => {
-    setAllClicks(allClicks.concat("R"));
-    setRight(right + 1);
-  };
-
-  return (
-    <div>
-      <p>{left}</p>
-      <Button onClick={handleLeftClick} text="left" />
-      <p>{right}</p>
-      <Button text={"right"} onClick={handleRightClick} />
-      <History allClicks={allClicks} />
-    </div>
-  );
-};
-
-ReactDOM.render(<App2 />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+
+const Filter = (props) => (
+  <input onChange={props.handleSearch} value={props.search} />
+);
+
+const PersonForm = (props) => (
+  <form onSubmit={props.handleSubmit}>
+    <div>
+      <label>Name: </label>{" "}
+      <input value={props.newName} onChange={props.handleChange} />
+    </div>
+    <div>
+      <label>Number: </label>
+      <input value={props.newNumber} onChange={props.handleNumberChange} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+);
+
+const Persons = (props) => (
+  <div>
+    {props.toShow.map((person) => (
+      <p key={person.name}>
+        {person.name} {person.number}
+      </p>
+    ))}
+  </div>
+);
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [search, setNewSearch] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+    };
+    let found;
+    persons.forEach((person) => {
+      if (person.name.toLowerCase() === newName.toLowerCase()) {
+        alert(`${newName} is already added to phonebook`);
+        found = true;
+      }
+    });
+
+    if (found) {
+      return;
+    }
+
+    console.log(persons);
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    setNewSearch(e.target.value);
+  };
+
+  const toShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter search={search} handleSearch={handleSearch} />
+      <PersonForm
+        handleChange={handleChange}
+        handleNumberChange={handleNumberChange}
+        handleSubmit={handleSubmit}
+        newName={newName}
+        newNumber={newNumber}
+      />
+      <h2>Numbers</h2>
+      <Persons toShow={toShow} />
+    </div>
+  );
+};
+
+export default App;
