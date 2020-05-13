5abc8fe7951d24ea3eeeca9e9cd27557c1d03f78
Mike-Mp <devmichalis@protonmail.com>
2020-05-13 16:09:55 +0300

    use weatherstack

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState([]);
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const countriesToShow = countries.filter((country) =>
+    country.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  let countriesMap;
+  if (countriesToShow.length > 10) {
+    countriesMap = <p>Too many matches, specify another filter</p>;
+  } else if (countriesToShow.length > 1) {
+    countriesMap = countriesToShow.map((country) => (
+      <>
+        <p key={country.name}>{country.name}</p>
+      </>
+    ));
+  } else {
+    countriesMap = countriesToShow.map((country) => (
+      <>
+        <h2 key={country.name}>{country.name}</h2>
+        <p>capital: {country.capital}</p>
+        <p>population: {country.population}</p>
+        <h3>languages</h3>
+        <ul>
+          {country.languages.map((lang) => (
+            <li>{lang.name}</li>
+          ))}
+        </ul>
+        <img src={country.flag} alt={country.name}></img>
+      </>
+    ));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={handleChange} value={searchTerm} />
     </div>
   );
 }
