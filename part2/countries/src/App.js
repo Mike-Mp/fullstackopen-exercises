f1b5c5d55797205c0fba808c67361bc8e8bf1461
Mike-Mp <devmichalis@protonmail.com>
2020-05-13 16:59:40 +0300

    begin compartmentalizing app

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,7 +4,23 @@ import axios from "axios";
 
 function App() {
   const [countries, setCountries] = useState([]);
-  const [searchTerm, setSearchTerm] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then((res) => setCountries(res.data));
+  }, []);
+
+  useEffect(() => {
+    const api_key = process.env.REACT_APP_API_KEY;
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${api_key}&query=${countriesToShow[0].capital}`
+      )
+      .then((res) => setWeatherInfo(res.data));
+  }, [countriesToShow[0].capital]);
 
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
@@ -18,30 +34,33 @@ function App() {
     countriesMap = <p>Too many matches, specify another filter</p>;
   } else if (countriesToShow.length > 1) {
     countriesMap = countriesToShow.map((country) => (
-      <>
-        <p key={country.name}>{country.name}</p>
-      </>
+      <div key={country.name}>
+        <p>{country.name}</p>
+        <button onClick={() => setSearchTerm(country.name)}>show</button>
+      </div>
     ));
   } else {
     countriesMap = countriesToShow.map((country) => (
-      <>
-        <h2 key={country.name}>{country.name}</h2>
+      <div key={country.name}>
+        <h2>{country.name}</h2>
         <p>capital: {country.capital}</p>
         <p>population: {country.population}</p>
         <h3>languages</h3>
         <ul>
           {country.languages.map((lang) => (
-            <li>{lang.name}</li>
+            <li key={lang.name}>{lang.name}</li>
           ))}
         </ul>
         <img src={country.flag} alt={country.name}></img>
-      </>
+        {weatherInfo ? <div>{weatherInfo.location.name}</div> : <div></div>}
+      </div>
     ));
   }
 
   return (
     <div className="App">
       <input onChange={handleChange} value={searchTerm} />
+      <>{countriesMap}</>
     </div>
   );
 }
